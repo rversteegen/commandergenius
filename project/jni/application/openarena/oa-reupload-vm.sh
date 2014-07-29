cdd22f60b4f254350a1d7047943dedfba126f216
Bob the Hamster <Bob@HamsterRepublic.com>
2014-07-29 09:36:17 -0700

    Trying to remove non-ohrrpgce projects from ohrrpgce branch

--- project/jni/application/openarena/oa-reupload-vm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-adb shell rm /sdcard/Android/data/ws.openarena.sdl/files/libsdl-DownloadFinished-10.flag
-[ -n "$1" ] && adb shell rm -r /sdcard/Android/data/ws.openarena.sdl/files/.openarena
--- project/jni/application/vcmi/AndroidBuild.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# With default -O3 optimization each G++ process eats 2Gb RAM, so tone optimization down to -O2, and limit make to two jobs
-make -j2 2>&1 | tee build.log 
-[ -f libapplication.so ] || exit 1
-exit 0
