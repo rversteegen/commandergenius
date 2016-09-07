4ff10a6f93269e49a5b7ecb6dc29a36a4a46bccb
Ralph Versteegen <teeemcee@gmail.com>
2016-09-07 14:37:44 +1200

    The NDK version r12b supports a minimum of API level 9 (Android 2.3); switch to that otherwise it defaults to latest API.

--- project/jni/Application.mk
+++ project/jni/Application.mk
@@ -5,7 +5,7 @@ include jni/Settings.mk
 APP_STL := gnustl_static
 #APP_CFLAGS := -O3 -DNDEBUG -g # arm-linux-androideabi-4.4.3 crashes in -O0 mode on SDL sources
 APP_CFLAGS := -g # arm-linux-androideabi-4.4.3 crashes in -O0 mode on SDL sources
-APP_PLATFORM := android-4  # android 1.6
+APP_PLATFORM := android-9  # android 2.3
 APP_PIE := false # This feature makes executables incompatible to Android API 15 or lower
 
 # Global compiler flags
