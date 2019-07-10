24536f14ec43bfc23c8ac2fadb9da4626ef8ec2e
Ralph Versteegen <teeemcee@gmail.com>
2019-07-11 02:47:58 +1200

    Update APP_PLATFORM to android-9 to match minSdkVersion

--- project/jni/Application.mk
+++ project/jni/Application.mk
@@ -5,7 +5,9 @@ include jni/Settings.mk
 APP_STL := gnustl_static
 #APP_CFLAGS := -O3 -DNDEBUG -g # arm-linux-androideabi-4.4.3 crashes in -O0 mode on SDL sources
 APP_CFLAGS := -g # arm-linux-androideabi-4.4.3 crashes in -O0 mode on SDL sources
-APP_PLATFORM := android-4  # android 1.6
+APP_PLATFORM := android-9  # (android 2.3) Must be same as android:minSdkVersion (ideally) or smaller.
+                           # Warning: if this is smaller than your NDK version supports it
+                           # may default to some value larger than minSdkVersion!
 APP_PIE := false # This feature makes executables incompatible to Android API 15 or lower
 
 # Global compiler flags
